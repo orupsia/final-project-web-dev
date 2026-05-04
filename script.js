115df1994405c41eb4679b14b131402308278d28
relena <era8411@nyu.edu>
2026-05-04 13:26:19 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,86 @@
+let locationField;
+let infoField;
+let imgElement = document.getElementById("resultImage");
+
+let audio = new Audio(); // for ambient sounds
+
+document.addEventListener('DOMContentLoaded', () => {
+  locationField = document.querySelector("#location");
+  infoField = document.getElementById('info');
+
+  // background upload
+  document.getElementById("bgUpload").addEventListener("change", function(e) {
+    const file = e.target.files[0];
+    if (file) {
+      const url = URL.createObjectURL(file);
+      document.body.style.backgroundImage = `url(${url})`;
+    }
+  });
+});
+
+function getWeather(){
+  let location = locationField.value;
+
+  fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=YH5QF5SWJL5LZ8SFPZFT2RCC2`)
+    .then(res => res.json())
+    .then(data => {
+
+      let today = data.days[0];
+      let temp = today.temp;
+      let condition = today.conditions;
+
+      // text info
+      infoField.innerHTML = `
+        🌍 ${location}<br>
+        🌡️ Temp: ${temp}°F<br>
+        ☁️ Condition: ${condition}
+      `;
+
+      // simple forecast
+      infoField.innerHTML += `<br>📅 Today High: ${today.tempmax}°F | Low: ${today.tempmin}°F`;
+
+      // visuals + sounds
+      handleWeather(condition, temp);
+
+    })
+    .catch(err => {
+      console.error(err);
+      infoField.innerHTML = "couldn't find that location 😢";
+    });
+}
+
+function handleWeather(condition, temp){
+
+  condition = condition.toLowerCase();
+
+  // stop previous sound
+  audio.pause();
+  audio.currentTime = 0;
+
+  if (condition.includes("rain")) {
+    imgElement.src = "images/rainyday.png";
+    audio.src = "sounds/rainsounds.mp3";
+  }
+  else if (condition.includes("snow")) {
+    imgElement.src = "images/snowday.png";
+    audio.src = "sounds/windsounds.mp3";
+  }
+  else if (condition.includes("cloud")) {
+    imgElement.src = "images/cloudyday.png";
+    audio.src = "sounds/windsounds.mp3";
+  }
+  else if (condition.includes("clear")) {
+    imgElement.src = "images/sunnday.png";
+    audio.src = "sounds/birdsounds.mp3";
+  }
+  else {
+    imgElement.src = "images/default.png";
+  }
+
+  audio.loop = true;
+  audio.play();
+}
+
+function stopSound(){
+  audio.pause();
+}
