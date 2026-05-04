eedef2a309f7b042052ef8b3a113986d307ca991
relena <era8411@nyu.edu>
2026-05-04 14:32:54 -0400

    changed background and updated style.css

--- script.js
+++ script.js
@@ -1,24 +1,11 @@
 let locationField;
 let infoField;
-let imgElement = document.getElementById("resultImage");
 
 let audio = new Audio();
 
 document.addEventListener('DOMContentLoaded', () => {
   locationField = document.querySelector("#location");
   infoField = document.getElementById('info');
-
-  // background upload
-  document.getElementById("bgUpload").addEventListener("change", function(e) {
-    const file = e.target.files[0];
-    if (file) {
-      const url = URL.createObjectURL(file);
-
-      document.body.style.backgroundImage = `url(${url})`;
-      document.body.style.backgroundSize = "cover";
-      document.body.style.backgroundPosition = "center";
-    }
-  });
 });
 
 function getWeather(){
@@ -55,25 +42,33 @@ function handleWeather(condition){
   audio.pause();
   audio.currentTime = 0;
 
+  let bgImage = "";
+
   if (condition.includes("rain")) {
-    imgElement.src = "images/rainyday.png";
+    bgImage = "images/rainyday.png";
     audio.src = "sounds/rainsounds.mp3";
   }
   else if (condition.includes("snow")) {
-    imgElement.src = "images/snowday.png";
+    bgImage = "images/snowday.png";
     audio.src = "sounds/windsounds.mp3";
   }
   else if (condition.includes("cloud")) {
-    imgElement.src = "images/cloudyday.png";
+    bgImage = "images/cloudyday.png";
     audio.src = "sounds/windsounds.mp3";
   }
   else if (condition.includes("clear")) {
-    imgElement.src = "images/sunnday.png";
+    bgImage = "images/sunnday.png";
     audio.src = "sounds/birdsounds.mp3";
   }
   else {
-    imgElement.src = "images/default.png";
+    bgImage = "images/default.png";
+    audio.src = "";
   }
+
+  // ✅ BACKGROUND ONLY (no center image)
+  document.body.style.backgroundImage = `url('${bgImage}')`;
+  document.body.style.backgroundSize = "cover";
+  document.body.style.backgroundPosition = "center";
 
   audio.loop = true;
   audio.play();
@@ -81,9 +76,6 @@ function handleWeather(condition){
 
 function stopSound(){
   audio.pause();
-}
-
-function resetBackground(){
-  document.body.style.backgroundImage = "none";
-  document.body.style.backgroundColor = "white";
+  audio.currentTime = 0;
+  audio.src = "";
 }
